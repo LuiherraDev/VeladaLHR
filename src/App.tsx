1d73c975789808992a9de833ba245280d6ba9b99
luis <luiherra25@gmail.com>
2026-03-23 16:40:48 +0100

    Restructuring the project folders and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from 'react-router'
-import { MainLayout } from './layouts/MainLayout'
-import Home from './pages/Home'
+
+import Home from './pages/MainLayout'
 import NotFound from './pages/NotFound'
 
 function App() {
 
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
